1ff491c60ca83acf5371f41c29c1747ef54da699
zhlyakh <zhlyakh@gmail.com>
2021-05-15 21:36:06 +0300

    04_Liakh

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
-let firstArray = [];
-// console.log(typeof firstArray);
-firstArray = [123n, undefined, null, Symbol('Bio')];
+//the data given to us by the condition.
 
-firstArray.push(prompt('Введите слово'));
-firstArray.push(+prompt('Введите число'));
-firstArray.push(confirm('Do you conrirm?'));
-firstArray.push({age:+prompt('enter the age'), name:prompt('enter the name')});
-console.log(firstArray);
+const data = [
+    null,
+    undefined,
+    false,
+    'string',
+    0,
+    9999999999999999n,
+    Symbol('id'),
+    { itIsObject: true },
+    [0, 11, 22, 33],
+];
+
+
+
+
+//the first task
+
+// for (let i = 0; i < data.length; i++) {
+//     if (!data[i]){
+//         console.log(`${data[i]}, index - ${i}`);
+//     }
+// }
+
+
+//the second task
+
+// for (let i = 0; i < data.length; i++) {
+//     if (typeof data[i] === "object"){
+//         console.log(data[i]+ ` , index - ${i}`);
+//     }
+// }
+
+
+//data for the third task
+
+let personInfo = {
+    firstName: null,
+    surName: null,
+    sex: null,
+    age: null,
+    weight: null,
+    height: null,
+    isMarried: null,
+    hasChildren: null,
+}
+
+// the third task
+
+personInfo.firstName = prompt('Please enter thy first name');
+personInfo.surName = prompt('Please enter thy last name');
+personInfo.sex = prompt('Please enter thy sex');
+personInfo.age = +prompt('How old art thou?');
+personInfo.weight = +prompt('what is thy weight?');
+personInfo.height = +prompt('how tall art thou?');
+personInfo.isMarried = confirm('Dost thou have a spouse?');
+personInfo.hasChildren = confirm('Dost thou have children?');
+
+console.log(personInfo);
